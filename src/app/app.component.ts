e94244bd9670d64bcbb2d57ab5b3e3ede431ffe0
Adil <satilganov.adil@gmail.com>
2026-03-18 15:14:12 +0500

    feat homework15(header)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import './training';
+import { Color } from '../enums/Color';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import './training';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  public readonly companyName = 'РУМТИБЕТ';
+
+  constructor() {
+    this.saveLastVisit();
+    this.saveVisitCount();
+  }
+
+  isPrimaryColor(color: string): boolean {
+    return Object.values(Color).includes(color as Color);
+  }
+
+  saveLastVisit(): void {
+    const now = new Date().toISOString();
+    localStorage.setItem('lastVisit', now);
+  }
+
+  saveVisitCount(): void {
+    const visits = localStorage.getItem('visits');
+
+    const count = visits ? Number(visits) : 0;
+
+    localStorage.setItem('visits', String(count + 1));
+  }
+}
---
+++ src/enums/Color.ts
@@ -0,0 +1,5 @@
+export enum Color {
+  Red = 'rgb(255,0,0)',
+  Green = 'rgb(0,255,0)',
+  Blue = 'rgb(0,0,255)',
+}
